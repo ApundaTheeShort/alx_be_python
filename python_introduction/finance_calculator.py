bffab45e013ffa86abdbb8ae1d895248099e1db8
Apunda <apundawyclife333@gmail.com>
2025-07-10 17:13:01 +0300

    savings calculator 2.0

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,4 @@
-import locale
 
-locale.setlocale(locale.LC_ALL, '')
 
 monthly_income = float(input("Enter your monthly income: "))
 monthly_expenses = float(input("Enter your total monthly expenses: "))
@@ -8,11 +6,6 @@ monthly_expenses = float(input("Enter your total monthly expenses: "))
 monthly_savings = monthly_income - monthly_expenses
 projected_1year_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
-# sets locale to users' default settings
-formated_savings = locale.currency(monthly_savings, grouping=True, symbol=True)
-formated_yearly_savings = locale.currency(
-    projected_1year_savings, grouping=True, symbol=True)
-
-print("Your monthly savings are ", formated_savings)
-print("Projected savings after one year, with interest is: ",
-      formated_yearly_savings)
+print(f"Your monthly savings are: ${monthly_savings:.2f} ")
+print(
+    f"Projected savings after one year, with interest is: ${projected_1year_savings:.2f}")
