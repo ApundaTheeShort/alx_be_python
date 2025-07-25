8fa79e18baac226649c6f44209f0561e191b8f2c
Apunda <apundawyclife333@gmail.com>
2025-07-25 22:10:59 +0300

    updated arithmetic operations function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,3 @@
-# from arithmetic_operations import
-
-
 def perform_operation(num1, num2, operations):
     """Fn takes two operators and perform functions on them"""
 
@@ -16,8 +13,3 @@ def perform_operation(num1, num2, operations):
                 return "Can't divide by zero"
             else:
                 return num1 / num2
-
-
-if __name__ == "__main__":
-    result = perform_operation(5, 0, "multiply")
-    print("Result:", result)
