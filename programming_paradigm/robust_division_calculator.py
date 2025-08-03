6d0ccd84787d8f654d65c174f3c994427a79649a
Apunda <apundawyclife333@gmail.com>
2025-08-03 21:43:47 +0300

    Robust div calc

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -4,7 +4,7 @@ def safe_divide(numerator, denominator):
             raise ZeroDivisionError
         else:
             result = float(numerator) / float(denominator)
-            return f"{result:.2f}"
+            return f"{result:.1f}"
 
     except ValueError:
         print("Error: Please enter numeric values only.")
