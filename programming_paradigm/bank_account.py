1e115a5472468ce1f4edf8f12fbaa2fbaabcfc20
Apunda <apundawyclife333@gmail.com>
2025-08-03 20:22:12 +0300

    bank account program

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,36 @@
+class InsuficientFundsError(Exception):
+    pass
+    # def __init__(self):
+    #     pass
+
+    # def __str__(self):
+    #     # return "Insufficient Funds"
+    #     pass
+
+
+class BankAccount:
+    def __init__(self, initial_balance):
+        self.initial_balance = initial_balance
+        self.account_balance = initial_balance
+
+    def deposit(self, amount):
+        balance = amount + self.account_balance
+        return balance
+
+    def withdraw(self, amount):
+        if amount > self.account_balance:
+            raise InsuficientFundsError("Insufficient Funds")
+        else:
+            balance = self.account_balance - amount
+            return balance
+
+    def display_balance(self):
+        print(self.account_balance)
+        return self.account_balance
+
+
+# try:
+#     account = BankAccount(100)
+#     account.withdraw(account)
+# except InsuficientFundsError as e:
+#     print(e)
